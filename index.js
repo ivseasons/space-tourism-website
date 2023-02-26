99d20acc4b8ade7ed6acdee1a7ce5b8a471ab88b
ivseasons <92192665+ivseasons@users.noreply.github.com>
2023-02-26 15:16:26 +0900

    draft

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Navigation 
 const toggleBtn = document.getElementById('open-btn');
 const nav = document.getElementById('primary-navigation');
 const changeAria = document.getElementById('aria')
@@ -16,3 +17,55 @@ toggleBtn.addEventListener('click', () => {
         changeAria.setAttribute('aria-expanded', false)
     }
 })
+
+// Tabs
+
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = document.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown', changeTabFocus);
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabContent)
+})
+
+let tabFocus = 0;
+
+function changeTabFocus(event) {
+    const keyLeft = 37;
+    const keyRight = 39;
+
+    if(event.keyCode === keyRight || event.keyCode === keyLeft) {
+        tabs[tabFocus].setAttribute('tabindex', -1)
+    }
+    if(event.keyCode === keyRight) {
+        tabFocus++;
+        if(tabFocus >= tabs.length) {
+            tabFocus = 0;
+        }
+    }
+    if(event.keyCode === keyLeft) {
+        tabFocus--;
+        if(tabFocus < 0) {
+            tabFocus = tab.length - 1;
+        }
+    }
+
+    tabs[tabFocus].setAttribute('tabindex', 0);
+    tabs[tabFocus].focus();
+}
+
+function changeTabContent(event) {
+    const targetTab = event.target;
+    const targetContent = targetTab.getAttribute('aria-controls');
+
+    const tabContainer = targetTab.parentNode;
+    const mainContainer = tabContainer.parentNode
+
+    mainContainer
+        .querySelectorAll('[role="tabcontent"]')
+        .forEach((content) => content.setAttribute("hidden", true));
+
+
+    mainContainer.querySelector(`#${targetContent}`).removeAttribute('hidden')
+}
